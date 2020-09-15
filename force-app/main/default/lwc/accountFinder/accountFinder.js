37306525e32601185da37fe1a8f589a62b264e92
Oscar Pachano <opachano@gmail.com>
2020-09-15 14:53:56 -0400

    Added AuraEnabled Class and logic

--- force-app/main/default/lwc/accountFinder/accountFinder.js
+++ force-app/main/default/lwc/accountFinder/accountFinder.js
@@ -1,6 +1,7 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, wire } from 'lwc';
+import queryAccountsByRevenue from '@salesforce/apex/AccountListControllerLwc.queryAccountsByRevenue';
 export default class AccountFinder extends LightningElement {
+  @wire(queryAccountsByRevenue, { annualRevenue: '$annualRevenue' }) accounts;
   annualRevenue = null;
   handleChange(event){
     this.annualRevenue = event.detail.value;
